df368f63a8e360f8bb291e3ef05b87ee92804781
Muhammed Hasan Ince <mhasanince@gmail.com>
2025-06-10 00:41:08 +0300

    feat(JIRA-007): add card component (#7)
    * feat(JIRA-007): add card component
    
    * delete
    
    * update
    
    * changeset

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-import { Button, range } from '@/lib';
+import { Button, Card, range } from '@/lib';
 import { Header } from '@/src/components/Header';
 
 function App() {
@@ -9,6 +9,10 @@ function App() {
   return (
     <main>
       <Header />
+      <Card
+        title='Lorem ipsum dolor sit amet.'
+        description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem fuga, repellat saepe numquam eligendi eius impedit? Placeat alias corrupti consequuntur ab impedit? Excepturi ipsa placeat totam alias accusamus illo expedita?'
+      />
       <Button
         className='bg-blue-500 text-white'
         onClick={() => setCount((prev) => [...prev, prev.length])}
